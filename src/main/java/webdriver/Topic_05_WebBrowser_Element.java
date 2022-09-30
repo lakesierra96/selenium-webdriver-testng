f21962eafd5b20c1373148cf2d4547c37f8c756a
lakesiera96 <you@example.com>
2022-09-30 19:10:12 +0700

    Update code

--- src/main/java/webdriver/Topic_05_WebBrowser_Element.java
+++ src/main/java/webdriver/Topic_05_WebBrowser_Element.java
@@ -2,6 +2,7 @@ package webdriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -13,30 +14,47 @@ import java.util.concurrent.TimeUnit;
 import static support.Utils.*;
 
 public class Topic_05_WebBrowser_Element {
-        String projectPath = System.getProperty("user.dir");
+    String projectPath = System.getProperty("user.dir");
 
-        @BeforeClass
-        public void beforeClass() {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-            driver.manage().window().maximize();
-            driver.get("http://live.techpanda.org/");
-        }
+    @BeforeClass
+    public void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://live.techpanda.org/");
+    }
 
-        @Test
-        public void TC_01_Navigate_Function() {
-            driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
-            driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
-            Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
-            driver.navigate().back();
-            Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
-            driver.navigate().forward();
-            Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
-        }
+    @Test
+    public void TC_01_Navigate_Function() {
+        driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+        driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+        driver.navigate().back();
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+        driver.navigate().forward();
+        Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
+    }
 
-        @AfterClass
-        public void afterClass() {
-//            driver.quit();
+    @Test
+    public void TC_04_MailChimp() {
+        driver.get("https://login.mailchimp.com/signup");
+        driver.findElement(By.id("email")).sendKeys("laksierra96@gmail.com");
+        WebElement passwordTextbox = driver.findElement(By.name("password"));
+        passwordTextbox.sendKeys("aaa");
+        sleepInSecond(2000);
+    }
+
+    public void sleepInSecond(long miliSecond) {
+        try {
+            Thread.sleep(miliSecond * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
+
+    @AfterClass
+    public void afterClass() {
+//            driver.quit();
+    }
+}
