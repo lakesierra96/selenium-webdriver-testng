da23bbc3b998e32e839a7ab5254d6d7bc7902c95
lakesiera96 <you@example.com>
2022-10-10 15:50:04 +0700

    Update code

---
+++ src/main/java/javaTester/Topic_04_Random_Number.java
@@ -0,0 +1,5 @@
+package javaTester;
+
+public class Topic_04_Random_Number {
+
+}
--- src/main/java/webdriver/Topic_07_Default_DropDown.java
+++ src/main/java/webdriver/Topic_07_Default_DropDown.java
@@ -4,129 +4,70 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static support.Utils.*;
 
 public class Topic_07_Default_DropDown {
     String projectPath = System.getProperty("user.dir");
+    Select select;
+    Random random;
 
     @BeforeClass
     public void beforeClass() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        random = new Random();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
     @Test
-    public void TC_01_Is_Displayed() {
-        driver.get("https://automationfc.github.io/basic-form/index.html");
-        if (driver.findElement(By.id("mail")).isDisplayed()) {
-            driver.findElement(By.id("mail")).sendKeys("Automation");
-            System.out.println("Display");
-        } else {
-            System.out.println("Element is not displayed");
-        }
-        if (driver.findElement(By.xpath("//label[text()='Under 18']")).isDisplayed()) {
-            driver.findElement(By.id("under_18")).click();
-            System.out.println("Display");
+    public void TC_01_Default_DropDown() {
+        driver.get("https://demo.nopcommerce.com/register");
+        sendKey(By.cssSelector("input#FirstName"), "Hugh");
+        sendKey(By.id("LastName"), "Pham");
 
-        } else {
-            System.out.println("Element is not displayed");
-        }
-        if (driver.findElement(By.id("edu")).isDisplayed()) {
-            driver.findElement(By.id("edu")).sendKeys("Automation");
-            System.out.println("Display");
-        } else {
-            System.out.println("Element is not displayed");
-        }
-        if (driver.findElement(By.xpath("//h5[text()='Name: User5']")).isDisplayed()) {
-            System.out.println("Display");
-        } else {
-            System.out.println("Element is not displayed");
-        }
-    }
+        select = new Select(getElement(By.name("DateOfBirthDay")));
+        select.selectByVisibleText("17");
 
-    @Test
-    public void TC_02_MailChimp() {
-        visit("https://login.mailchimp.com/signup");
-        WebElement emailTextbox = getElement(By.id("email"));
-        emailTextbox.sendKeys("hieu.pkt96@gmail.com");
-        sleepInSecond(1);
-        click((By.name("username")));
-        sleepInSecond(2);
+        select = new Select(getElement(By.name("DateOfBirthMonth")));
+        select.selectByVisibleText("August");
 
-        //Get entered text from a textbox
-        String usernameText = getElement(By.name("username")).getAttribute("value");
-        Assert.assertEquals(usernameText, "hieu.pkt96@gmail.com");
+        select = new Select(getElement(By.name("DateOfBirthYear")));
+        select.selectByVisibleText("1996");
 
-        WebElement passwordTextbox = getElement(By.id("new_password"));
-        passwordTextbox.sendKeys("aaa");
-        Assert.assertTrue(getElement(By.xpath("//li[@class='lowercase-char completed']")).isDisplayed());
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='uppercase-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='number-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='special-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='8-char not-completed']")));
+        String email = "hieu.pkt" + random.nextInt(999) + "@gmail.com";
+        sendKey(By.id("Email"), email);
+        sendKey(By.id("Password"), "123456?a");
+        sendKey(By.id("ConfirmPassword"), "123456?a");
+        click(By.id("register-button"));
+        Assert.assertEquals(getText(By.cssSelector("div.result")), "Your registration completed");
 
-        passwordTextbox.clear();
-        passwordTextbox.sendKeys("AAA");
-        Assert.assertTrue(getElement(By.xpath("//li[@class='lowercase-char not-completed']")).isDisplayed());
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='uppercase-char completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='number-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='special-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='8-char not-completed']")));
+        click(By.linkText("My account"));
 
-        passwordTextbox.clear();
-        passwordTextbox.sendKeys("123");
-        Assert.assertTrue(getElement(By.xpath("//li[@class='lowercase-char not-completed']")).isDisplayed());
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='uppercase-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='number-char completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='special-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='8-char not-completed']")));
-        isDisplayed((By.xpath("//li[@class='8-char not-completed']")));
+        select = new Select(getElement(By.name("DateOfBirthDay")));
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "17");
 
-        passwordTextbox.clear();
-        passwordTextbox.sendKeys("!@#");
-        Assert.assertTrue(getElement(By.xpath("//li[@class='lowercase-char not-completed']")).isDisplayed());
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='uppercase-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='number-char not-completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='special-char completed']")));
-        Assert.assertTrue(isDisplayed(By.xpath("//li[@class='8-char not-completed']")));
+        select = new Select(getElement(By.name("DateOfBirthMonth")));
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "August");
 
-        passwordTextbox.clear();
-        passwordTextbox.sendKeys("Hieu@123");
+        select = new Select(getElement(By.name("DateOfBirthYear")));
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "1996");
 
-        //Hàm này check nếu ko display thì in ra console
-        isElementPresent(By.xpath("//li[@class='lowercase-char not-completed']"));
-
-
-        //Hàm này sẽ check xem thỏa điều kiện của password chưa, các error có còn hiện ko
-        List<WebElement> errorMess = driver.findElements(By.xpath("//li[contains(@class,'not-completed')]"));
-        if (errorMess.size() > 0){
-            System.out.println("Element is present. ");
-        }
-        else {
-            System.out.println("Element is not present. ");
-        }
-    }
-
-    public void sleepInSecond(long miliSecond) {
-        try {
-            Thread.sleep(miliSecond * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Assert.assertEquals(getAttribute(By.id("Email"), "value"), email);
     }
 
     @AfterClass
     public void afterClass() {
-            driver.quit();
+//            driver.quit();
     }
 }
