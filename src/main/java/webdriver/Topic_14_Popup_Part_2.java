f642bad27dcc53c52b9d7195c5b699e36d4136b9
Lakesierra <hieupkt@devblock.net>
2023-03-14 04:52:22 +0700

    Update new code

---
+++ src/main/java/webdriver/Topic_14_Popup_Part_2.java
@@ -0,0 +1,63 @@
+package webdriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static support.Utils.*;
+
+public class Topic_14_Popup_Part_2 {
+    String osName = System.getProperty("os.name");
+    WebDriverWait wait;
+    Actions act;
+
+    @BeforeClass
+    public void beforeClass() {
+        //Disable chrome notification
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, 5);
+        act = new Actions(driver);
+
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_Fixed_Popup_NgoaiNgu() {
+        visit("https://www.javacodegeeks.com/");
+
+        By lePopup = By.cssSelector("div.lepopup-popup-container>div:not([style^='display:none'])");
+        Assert.assertTrue(isDisplayed(lePopup));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        //driver.quit();
+    }
+
+
+    public boolean isElementSelected(By by) {
+        return getElement(by).isSelected();
+    }
+
+    public void sleepInSecond(long miliSecond) {
+        try {
+            Thread.sleep(miliSecond * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
