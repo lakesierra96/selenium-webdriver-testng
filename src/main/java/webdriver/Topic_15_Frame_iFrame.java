b8063d7488e709601c14a4666799d287b60517cc
Lakesierra <hieupkt@devblock.net>
2023-06-21 03:33:58 +0700

    Update new code

--- src/main/java/webdriver/Topic_15_Frame_iFrame.java
+++ src/main/java/webdriver/Topic_15_Frame_iFrame.java
@@ -3,6 +3,7 @@ package webdriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
@@ -13,6 +14,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -65,6 +67,18 @@ public class Topic_15_Frame_iFrame {
         //cách để khai báo hàm select nhanh khi mà chỉ cần dùng select 1 lần (nên khai báo biến khi xài cho nhiều lần)
         new Select(getElement(By.id("serviceSelect"))).selectByVisibleText("HỖ TRỢ KỸ THUẬT");
         sendKey(By.name("message"), "test");
+
+        driver.switchTo().defaultContent();
+
+        String keyword = "Excel";
+        sendKey(By.id("live-search-bar"), keyword);
+        click(By.cssSelector("button.search-button"));
+
+        List <WebElement> courseName = getElements(By.cssSelector("div.content>h4"));
+        for (WebElement course : courseName) {
+            System.out.println(course.getText());
+            Assert.assertTrue(course.getText().contains(keyword));
+        }
     }
 
     @AfterClass
