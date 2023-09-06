979d09ccbb1b80f72aca251e180a9f2fce503acd
lakesiera96 <you@example.com>
2023-09-06 19:13:30 +0700

    Update code

--- src/main/java/webdriver/Topic_16_Windows_Tab.java
+++ src/main/java/webdriver/Topic_16_Windows_Tab.java
@@ -59,12 +59,18 @@ public class Topic_16_Windows_Tab {
     @Test
     public void TC_03_Greater_Two_Tabs() {
         visit("http://live.techpanda.org/");
+        String parentID = driver.getWindowHandle();
         click(By.xpath("//a[text()='Mobile']"));
         click(By.xpath("//a[@title='Sony Xperia']/parent::h2/following-sibling::div/button/following-sibling::ul//li/a[text()='Add to Compare']"));
         Assert.assertTrue(isDisplayed(By.xpath("//span[text()='The product Sony Xperia has been added to comparison list.']")));
 
         click(By.xpath("//a[@title='Samsung Galaxy']/parent::h2/following-sibling::div/button/following-sibling::ul//li/a[text()='Add to Compare']"));
         Assert.assertTrue(isDisplayed(By.xpath("//span[text()='The product Samsung Galaxy has been added to comparison list.']")));
+
+        click(By.xpath("//button[@title='Compare']"));
+        switchToWindowByTitle("Products Comparison List - Magento Commerce");
+        Assert.assertEquals(driver.getTitle(), "Products Comparison List - Magento Commerce");
+        closeAllWindowsExceptParentID(parentID);
     }
 
     @AfterClass
