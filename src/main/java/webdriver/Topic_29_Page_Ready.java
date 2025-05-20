1e5c8308763cb53f894962a228a477dbd6b28780
lakesiera96 <you@example.com>
2025-05-21 02:48:34 +0700

    Update code

--- src/main/java/webdriver/Topic_29_Page_Ready.java
+++ src/main/java/webdriver/Topic_29_Page_Ready.java
@@ -1,9 +1,8 @@
 package webdriver;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -12,27 +11,38 @@ import org.testng.annotations.Test;
 
 import static support.Utils.*;
 
-import java.util.concurrent.TimeUnit;
-
 public class Topic_29_Page_Ready {
-        WebDriver driver;
-        WebDriverWait wait;
+    WebDriverWait wait;
 
-
-        @BeforeClass
-        public void beforeClass() {
-            openBrowser();
-            wait = new WebDriverWait(driver, 30);
-        }
-
-        @Test
-        public void TC_01_Orange_HRM_API() {
-            visit("https://api.orangehrm.com/");
-
-        }
-
-        @AfterClass
-        public void afterClass() {
-//            driver.quit();
-        }
+    @BeforeClass
+    public void beforeClass() {
+        openBrowser();
+        wait = new WebDriverWait(driver, 30);
     }
+
+    @Test
+    public void TC_01_Orange_HRM_API() {
+        visit("https://api.orangehrm.com/");
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.spinner")));
+        Assert.assertEquals("OrangeHRM REST API Documentation", getText(By.cssSelector("div#project h1")));
+    }
+
+    @Test
+    public void TC_02_Admin_NopCommerce() {
+        visit("https://admin-demo.nopcommerce.com");
+        click(By.cssSelector("div.buttons"));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div#ajaxBusy")));
+        click(By.xpath("//a[text()='Logout']"));
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+        //driver.quit();
+    }
+
+
+    public WebElement getElement(String locator) {
+        return driver.findElement(By.xpath(locator));
+    }
+}
