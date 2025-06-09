dbbbdfc10157c10f7e5facbec50e2cad3399f9bc
Lakesierra <hieupkt@devblock.net>
2025-06-10 02:31:49 +0700

    Update new code

---
+++ src/main/java/testng/mobileApp/Customer.java
@@ -0,0 +1,28 @@
+package testng.mobileApp;
+
+import org.testng.annotations.Test;
+
+public class Customer {
+    //CRUD: Create Read Update Delete
+    //API: Post Get Put Delete
+
+    @Test(groups = "mobile")
+    public void Customer_01_Create_New_Customer() {
+
+    }
+
+    @Test(groups = "mobile")
+    public void Customer_02_View_Customer() {
+
+    }
+
+    @Test(groups = "mobile")
+    public void Customer_03_Edit_Customer() {
+
+    }
+
+    @Test(groups = "mobile")
+    public void Customer_04_Edit_Customer() {
+
+    }
+}
---
+++ src/main/java/testng/webApp/Customer.java
@@ -0,0 +1,28 @@
+package testng.webApp;
+
+import org.testng.annotations.Test;
+
+public class Customer {
+    //CRUD: Create Read Update Delete
+    //API: Post Get Put Delete
+
+    @Test(groups = "web")
+    public void Customer_01_Create_New_Customer() {
+
+    }
+
+    @Test(groups = "web")
+    public void Customer_02_View_Customer() {
+
+    }
+
+    @Test(groups = "web")
+    public void Customer_03_Edit_Customer() {
+
+    }
+
+    @Test(groups = "web")
+    public void Customer_04_Delete_Customer() {
+
+    }
+}
