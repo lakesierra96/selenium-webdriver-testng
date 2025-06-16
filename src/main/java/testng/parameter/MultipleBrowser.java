5c57b2f8ff65a24eb2219d98501e6c6ecbd218f0
lakesiera96 <you@example.com>
2025-06-16 13:32:23 +0700

    Update code

--- src/main/java/testng/parameter/MultipleBrowser.java
+++ src/main/java/testng/parameter/MultipleBrowser.java
@@ -40,15 +40,31 @@ public class MultipleBrowser {
         driver.manage().window().maximize();
     }
 
+    @Parameters("environment")
     @Test
-    public void TC_01_Navigate_Function() {
-        visit("https://live.techpanda.org/index.php/customer/account/login/");
+    public void TC_01_Navigate_Function(String environmentName) {
+        visit(getEnvironmentName(environmentName) + "index.php/customer/account/login/");
         sendKey(By.id("email"), "selenium_11_01@gmail.com");
         sendKey(By.id("pass"), "111111");
         click(By.id("send2"));
         Assert.assertTrue(getText(By.xpath("//div[@class='col-1']//p")).contains("selenium_11_01@gmail.com"));
         click(By.xpath("//header[@id='header']//span[text()='Account']"));
         click(By.linkText("Log Out"));
+    }
+
+    public String getEnvironmentName(String environmentName) {
+        String url = null;
+
+        if (environmentName.toLowerCase().equals("dev")) {
+            url = "https://dev.techpanda.org/";
+        } else if (environmentName.toLowerCase().equals("staging")) {
+            url = "https://staging.techpanda.org/";
+        } else if (environmentName.toLowerCase().equals("live")) {
+            url = "https://live.techpanda.org/";
+        } else {
+            throw new RuntimeException("Env is invalid");
+        }
+        return url;
     }
 
     @AfterClass(alwaysRun = true)
