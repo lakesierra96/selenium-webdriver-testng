1fc26231609946e7a8f1699a4ef5eb66124df5a8
lakesiera96 <you@example.com>
2025-06-16 13:49:14 +0700

    Update code

---
+++ src/main/java/testng/webApp/Administrator.java
@@ -0,0 +1,28 @@
+package testng.webApp;
+
+import org.testng.annotations.Test;
+
+public class Administrator {
+    //CRUD: Create Read Update Delete
+    //API: Post Get Put Delete
+
+    @Test(groups = "web")
+    public void Admin_01_Create_New_Customer() {
+
+    }
+
+    @Test(groups = "web")
+    public void Admin_02_View_Customer() {
+
+    }
+
+    @Test(groups = "web")
+    public void Admin_03_Edit_Customer() {
+
+    }
+
+    @Test(groups = "web")
+    public void Admin_04_Delete_Customer() {
+
+    }
+}
